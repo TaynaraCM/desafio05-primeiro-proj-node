b59c844c7664eba52c549b7caf5c7de3972bf666
TaynaraCM <taynaracruvinel101@gmail.com>
2020-11-11 09:30:22 -0300

    passed all tests

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
 import Transaction from '../models/Transaction';
+import transactionRouter from '../routes/transaction.routes';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    let income = 0;
+    let outcome = 0;
+    const arrayOfTransactions = this.transactions;
+    arrayOfTransactions.forEach(transaction => {
+      if (transaction.type === 'income') {
+        income += transaction.value;
+      } else if (transaction.type === 'outcome') {
+        outcome += transaction.value;
+      } else {
+        throw Error('This is not a valid transaction');
+      }
+    });
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    // const balance =
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
